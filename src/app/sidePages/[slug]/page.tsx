f38201825d4fc8a863b8c628466978a8b446f57e
liebero3 <liebero3@gmail.com>
2025-06-09 16:12:10 +0200

    feat: support images in side pages

--- src/app/sidePages/[slug]/page.tsx
+++ src/app/sidePages/[slug]/page.tsx
@@ -2,9 +2,10 @@
 import Link from "next/link"
 import { useParams } from "next/navigation"
 import { useEffect, useState } from "react"
+import ReactMarkdown from "react-markdown"
 
 export default function sidePages(){
-    const [inhalt,setInhalt] = useState()
+    const [inhalt,setInhalt] = useState<string>()
     const { slug } = useParams()
     useEffect(()=>{
         async function posten(){
@@ -34,8 +35,17 @@ export default function sidePages(){
     },[slug])
     return(
         <>
-            <div>
-                {inhalt}
+            <div className=" max-w-150 lg:max-w-200 justify-self-center text-2xl">
+                {inhalt && (
+                    <ReactMarkdown
+                        components={{
+                            h2:({node,...props})=> <p className=" text-3xl text-justify my-5 font-bold" {...props}/>,
+                            p:({node,...props})=> <p className=" text-2xl text-justify" {...props}/>,
+                            img:({node,...props})=> <img className="my-4" {...props}/>
+                        }}>
+                        {inhalt}
+                    </ReactMarkdown>
+                )}
                 <Link href={"/"}>zurück</Link>
             </div>
         </>
